2e657aa7332bb4c2769bb09e99b5995da76c8f27
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-13 10:20:49 +0530

    new resource to create ami with inline commands

--- state.tf
+++ state.tf
@@ -1,10 +1,12 @@
 terraform {
-  backend "s3" {}
-  bucket           = "srimaan-terraform-state-files" //existing bucket name to store state files
-  key              = "ami/terraform.tfstate" //path in bucket
-  region           = "us-east-1"
-  dynamodb_table   = "terraform" //dynamodb table name
+  backend "s3" {
+    bucket           = "srimaan-terraform-state-files" //existing bucket name to store state files
+    key              = "ami/terraform.tfstate" //path in bucket
+    region           = "us-east-1"
+    dynamodb_table   = "terraform" //dynamodb table name
+  }
 }
+
 
 provider "aws" {
   region = "us-east-1"
