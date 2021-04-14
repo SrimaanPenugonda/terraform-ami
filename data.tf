22756b173cd23ed9690e1a0a98241b184c79dbe4
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-14 10:05:11 +0530

    create an ami from instance (aws_instance.ami_instance)

--- data.tf
+++ data.tf
@@ -1,8 +1,12 @@
 data "aws_ami" "ami" {
   most_recent   = true
   owners        = ["973714476881"]
+#  filter {
+#    name   = "image-id"
+#    values = ["ami-079a3f3cf00741286"]
+#  }  ami id will chanhe all time so filtering on name
   filter {
-    name   = "image-id"
-    values = ["ami-079a3f3cf00741286"]
+    name   = "name"
+    values = ["Centos-7-DevOps-Practice"]
   }
 }
