18fad0a0dcb3651e99f276b17c1709a711ae31a6
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-14 08:29:50 +0530

    added security grp to instance

--- instance.tf
+++ instance.tf
@@ -1,17 +1,42 @@
 resource "aws_instance" "ami_instance" {
   ami           = data.aws_ami.ami.id
   instance_type = "t3.small"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags           = {
       Name       = "${var.COMPONENT}-ami"
   }
 }
 
+//security grp
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-${var.COMPONENT}-ami-sg"
+  description = "Allow SSH inbound traffic to AMI"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [0.0.0.0/0] // internet
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-${var.COMPONENT}-ami-sg"
+  }
+}
 resource "null_resource" "provisioner" {
   provisioner "remote-exec" { // connect and execute commands in remote instance
     connection {
       host        = aws_instance.ami_instance.public_ip
       user        = "root"
-      password    = "DevOps321" //dont hardcode like this
+      password    = "DevOps321" //don't hardcode like this
     }
     inline        =[
       "yum install make -y",
