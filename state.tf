9e1429e0c4128de5371444ebd5a85b1f26efe752
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-18 08:15:33 +0530

    create an ami from instance (aws_instance.ami_instance)

--- state.tf
+++ state.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket           = "srimaan-terraform-state-files" //existing bucket name to store state files
-    key              = "ami/terraform.tfstate" //path in bucket
-    region           = "us-east-1"
-    dynamodb_table   = "terraform" //dynamodb table name
+#    bucket           = "srimaan-terraform-state-files" //existing bucket name to store state files
+#    key              = "ami/terraform.tfstate" //path in bucket
+#    region           = "us-east-1"
+#    dynamodb_table   = "terraform" //dynamodb table name
+    #Mention this at make file init to create diff path for every component in bucket(using COMPONENT)
   }
 }
 
